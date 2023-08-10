8ac9c1e5529e3bfd1adfc17c87c010aba667324f
MinY10 <1326657117@qq.com>
2023-08-10 15:14:45 +0800

    自动打开浏览器

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    open: true, // 打开浏览器
+  },
 })
